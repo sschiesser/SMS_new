cfc37b2abcfb49fe95dbe6853a7895455ea16fa5
Sébastien Schiesser <sebastien.schiesser@zhdk.ch>
2016-11-22 09:04:32 +0100

    Polling & sending (not 100% stable)... now working on central part

--- SMS_peripheral1/SMS_peripheral1/src/sms_v_0_2/inc/sms_i2c.h
+++ SMS_peripheral1/SMS_peripheral1/src/sms_v_0_2/inc/sms_i2c.h
@@ -18,15 +18,15 @@
  * MACROS
  * ------ */
 /* I2C master settings for MPU9250 IMU */
-#define SMS_I2C_MASTER_PORT             (I2C1)
+#define SMS_I2C_MASTER_PORT             (I2C0)
 #define SMS_I2C_MASTER_CLK_SRC          (I2C_CLK_INPUT_1)
 #define SMS_I2C_MASTER_CLK_DIV          (130)
 /* Pins */
-#define SMS_I2C_MASTER_PIN_SDA          (PIN_LP_GPIO_14)
-#define SMS_I2C_MASTER_PIN_SCL          (PIN_LP_GPIO_15)
+#define SMS_I2C_MASTER_PIN_SDA          (PIN_LP_GPIO_8)
+#define SMS_I2C_MASTER_PIN_SCL          (PIN_LP_GPIO_9)
 /* Mux */
-#define SMS_I2C_MASTER_MUX_SDA          (MUX_LP_GPIO_14_I2C1_SDA)
-#define SMS_I2C_MASTER_MUX_SCL          (MUX_LP_GPIO_15_I2C1_SCL)
+#define SMS_I2C_MASTER_MUX_SDA          (MUX_LP_GPIO_8_I2C0_SDA)
+#define SMS_I2C_MASTER_MUX_SCL          (MUX_LP_GPIO_9_I2C0_SCL)
 
 #define I2C_DATA_LENGTH                 (16)
 #define I2C_TIMEOUT                     (1000)
--- SMS_peripheral1/SMS_peripheral1/src/sms_v_0_2/inc/sms_mpu.h
+++ SMS_peripheral1/SMS_peripheral1/src/sms_v_0_2/inc/sms_mpu.h
@@ -29,9 +29,9 @@
 
 #define SMS_MPU_I2C_SLAVE_ADDR              (0x69)
 
-#define SMS_MPU_SAMPLE_RATE_HZ              (5)
-#define SMS_MPU_TEMP_MULTIPLIER             (5)
-#define SMS_MPU_COMPASS_MULTIPLIER          (5)
+#define SMS_MPU_SAMPLE_RATE_HZ              (1)
+#define SMS_MPU_TEMP_MULTIPLIER             (1)
+#define SMS_MPU_COMPASS_MULTIPLIER          (1)
 #define SMS_MPU_COMPASS_RATE_HZ             (SMS_MPU_SAMPLE_RATE_HZ / SMS_MPU_COMPASS_MULTIPLIER)
 
 #define SMS_MPU_ACCEL_ON                    (0x01)
